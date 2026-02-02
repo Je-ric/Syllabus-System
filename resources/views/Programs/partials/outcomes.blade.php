48b75c9e27c6c2b70d997dadd82ed9cadd9bbb1a
Je-ric <delacruz.jeric.j@gmail.com>
2026-02-02 11:09:49 +0800

    feat: enhance program outcomes and PEOs sections with descriptive text

--- resources/views/Programs/partials/outcomes.blade.php
+++ resources/views/Programs/partials/outcomes.blade.php
@@ -1,9 +1,10 @@
 <div class="bg-white border rounded p-4">
-    <h2 class="font-semibold mb-3">Program Outcomes (POs)</h2>
+    <h2 class="font-semibold mb-3">Program Outcomes (POs) and its Relationship to the Program Educational Objectives</h2>
 
     {{-- Show PEOs of the selected program for reference --}}
     <livewire:programs.peo-display :program="$program" />
 
+    <p>By the time of graduation, students of the program have the ability to:</p>
     {{-- Manage POs and map to PEOs --}}
     <livewire:programs.manage-pos :program="$program" />
 </div>
--- resources/views/Programs/partials/peos.blade.php
+++ resources/views/Programs/partials/peos.blade.php
@@ -1,5 +1,6 @@
 <div class="bg-white border rounded p-4">
     <h2 class="font-semibold mb-3">Program Educational Objectives (PEOs)</h2>
 
+    <p>Three to five years after graduation, the BSIT graduates are:</p>
     <livewire:programs.manage-peos :program="$program" />
 </div>
