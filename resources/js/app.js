c64c6bbeb33471824c2877fad89b99729bd3a9e1
Je-ric <delacruz.jeric.j@gmail.com>
2026-08-12 11:47:00 +0800

    refactor(app.js, head-assets): Comment out collapse import and initialization; enable Alpine.js collapse plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,14 @@ import './syllabus-wizard';
 // import './syllabus-steps-ui';
 
 // import Alpine from 'alpinejs';
-import collapse from '@alpinejs/collapse';
+// import collapse from '@alpinejs/collapse';
 // import Precognition from 'laravel-precognition-alpine'; // for input requests validation
 
 // window.Alpine = Alpine;
 
-document.addEventListener('alpine:init', () => {
-    Alpine.plugin(collapse);
-    // Alpine.plugin(Precognition);
-});
+// document.addEventListener('alpine:init', () => {
+//     Alpine.plugin(collapse);
+//     Alpine.plugin(Precognition);
+// });
 
 // Alpine.start();
--- resources/views/includes/head-assets.blade.php
+++ resources/views/includes/head-assets.blade.php
@@ -20,4 +20,12 @@
 {{-- <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script> --}}
 {{-- Alpine Core --}}
 {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
+
+<script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+
+<script>
+    document.addEventListener('alpine:init', () => {
+        Alpine.plugin(window.Collapse);
+    });
+</script>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
