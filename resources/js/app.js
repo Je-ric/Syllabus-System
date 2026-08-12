d50024ffc2ae2a1b769cb4fa67a08f0adaf27fdc
Je-ric <delacruz.jeric.j@gmail.com>
2026-08-12 11:12:11 +0800

    refactor(app.js): Comment out Alpine.js initialization and window assignment

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,11 +6,11 @@ import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 import Precognition from 'laravel-precognition-alpine'; // for input requests validation
 
-window.Alpine = Alpine;
+// window.Alpine = Alpine;
 
 document.addEventListener('alpine:init', () => {
     Alpine.plugin(Precognition);
     Alpine.plugin(collapse);
 });
 
-Alpine.start();
+// Alpine.start();
