7cb5680d8c08c7e68b469daff95c629a9300b11e
Je-ric <delacruz.jeric.j@gmail.com>
2026-08-12 10:16:27 +0800

    refactor(app.js): Reorganize Alpine.js imports and initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,22 +4,12 @@ import './syllabus-wizard';
 
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
-// import Precognition from 'laravel-precognition-alpine';
-
-// window.Alpine = Alpine;
-
-// Alpine.plugin(collapse);
-// Alpine.plugin(Precognition);
-
-// Alpine.start();
-// import Alpine from 'alpinejs';
 import Precognition from 'laravel-precognition-alpine'; // for input requests validation
 
 // window.Alpine = Alpine;
-// Alpine.plugin(Precognition);
-// Alpine.start();
-// import Precognition from 'laravel-precognition-alpine';
+Alpine.start();
 
 document.addEventListener('alpine:init', () => {
     Alpine.plugin(Precognition);
+    Alpine.plugin(collapse);
 });
