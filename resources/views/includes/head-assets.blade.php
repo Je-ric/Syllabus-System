1def508867622e08d62ae94798968bfcd7a99869
Je-ric <delacruz.jeric.j@gmail.com>
2026-08-12 08:42:55 +0800

    refactor(head-assets): Comment out Alpine.js script includes

--- resources/views/includes/head-assets.blade.php
+++ resources/views/includes/head-assets.blade.php
@@ -17,8 +17,8 @@
 
 @livewireStyles
 {{-- Alpine Plugins --}}
-<script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+{{-- <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script> --}}
 {{-- Alpine Core --}}
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+{{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
